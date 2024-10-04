e210ef124669ddb9fe894a19ad42ee8b0f57069c
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-10-04 08:52:52 -0300

    creating Dev and BootCamp

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,58 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+	
+	private String nome;
+	private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluido = new LinkedHashSet<>();
+	
+	public void inscreverBootCamp(BootCamp bc) {
+		conteudosInscritos.addAll(bc.getConteudos());
+		bc.getDevInscritos().add(this);
+		
+	}
+	
+	public void progredir() {
+		Optional<Conteudo> conteudo = conteudosInscritos.stream().findFirst();
+		if(conteudo.isPresent()) {
+			conteudosConcluido.add(conteudo.get());
+			conteudosInscritos.remove(conteudo.get());
+		}
+		
+	}
+
+	public double calcularTotalXP() {
+		return conteudosConcluido.stream().mapToDouble(conteudo -> conteudo.calcularXP()).sum();
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getConteudosInscritos() {
+		return conteudosInscritos;
+	}
+
+	public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+		this.conteudosInscritos = conteudosInscritos;
+	}
+
+	public Set<Conteudo> getConteudosConcluido() {
+		return conteudosConcluido;
+	}
+
+	public void setConteudosConcluido(Set<Conteudo> conteudosConcluido) {
+		this.conteudosConcluido = conteudosConcluido;
+	}
+	
+	
+}
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,24 +2,25 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 
-	private String titulo;
-	private String descricao;
+	
 	private LocalDate data;
-	public String getTitulo() {
-		return titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
+	
 	public LocalDate getData() {
 		return data;
 	}
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
 	}
+	@Override
+	public double calcularXP() {
+		
+		return XP_PADRAO + 20;
+	}
+	
+	
 	
 	
 }
